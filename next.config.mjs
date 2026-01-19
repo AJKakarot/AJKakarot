f95250214e708852b9ec160e00cf2b4b5f597e13
Divyansh Sharma <divyanshsharma4507@gmail.com>
2026-01-19 18:21:27 +0530

    Update title in roadmap and add redirects in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/blog/roadmap',
+        destination: 'https://dub.sh/free-dev-resources',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
